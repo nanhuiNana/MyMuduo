d774fd346b73ddb36349e3cbe6f17ffce6ce43a4
nanhui <3011940542@qq.com>
2024-03-19 16:39:14 +0800

    封装Poller默认具体实现

---
+++ src/DefaultPoller.cc
@@ -0,0 +1,12 @@
+#include "../include/EpollPoller.h"
+#include "../include/Poller.h"
+
+#include <stdlib.h>
+
+Poller *Poller::newDefaultPoller(EventLoop *loop) {
+    if (::getenv("MUDUO_USE_POLL")) {
+        return nullptr; // 生成poll的实例
+    } else {
+        return new EpollPoller(loop); // 生成epoll的实例
+    }
+}
