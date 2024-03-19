2edbf028a967a587c4e477e7e176393e36d11133
nanhui <3011940542@qq.com>
2024-03-19 16:35:58 +0800

    封装Poller具体实现

---
+++ src/Poller.cc
@@ -0,0 +1,9 @@
+#include "../include/Poller.h"
+Poller::Poller(EventLoop *loop)
+    : ownerLoop_(loop) {
+}
+
+bool Poller::hasChannel(Channel *channel) const {
+    auto it = channels_.find(channel->fd());
+    return it != channels_.end() && it->second == channel;
+}
